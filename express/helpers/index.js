22968087caf340350c29a4cfdb14b6716de4e6db
zabeer001 <binzabirtareq@gmail.com>
2025-07-15 15:01:43 +0600

    e

--- express/helpers/index.js
+++
@@ -1,9 +0,0 @@
-
-import { hashPassword ,hashMultiplePasswords} from './hashPassowrd.js';
-import { formatPaginationResponse } from './formatPaginationResponse.js';
-
-export {
-  hashPassword,
-  hashMultiplePasswords,
-  formatPaginationResponse
-};
--- express/services/categories/destroy.service.js
+++
@@ -1,9 +0,0 @@
-import Category from "../../models/category.model.js";
-
-export const destroyService = async (id) => {
-  const category = await Category.findByIdAndDelete(id);
-  if (!category) {
-    throw new Error('Category not found');
-  }
-  return category;
-};
